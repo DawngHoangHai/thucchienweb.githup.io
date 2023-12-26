ae425f5b174478a379265220d489b538736ab5c2
Dang Hoang Hai <you@example.com>
2023-12-26 16:01:38 +0700

    updateWeb

--- search-box/app.js
+++ search-box/app.js
@@ -1,7 +1,7 @@
 var btnSearch = document.querySelector('.search__box button');
 var seachBox = document.querySelector('.search__box');
 var input = seachBox.querySelector('.input__text');
-btnSearch.addEventListener('click', () => {
+btnSearch.addEventListener('mouseover', () => {
    seachBox.classList.toggle('click');
    input.focus();
 });
